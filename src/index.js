d4bf313e19e442cf37344421f322d63f804f6ead
eiichiro.takahashi <eiichiro.takahashi@o-11126-mac.local>
2018-08-21 15:51:47 +0900

    use components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import m from 'mithril';
-m.render(document.body, 'hello world');
+
+let User = {
+  name: 'aho',
+  changeName: name => {
+    User.name = name;
+  },
+};
+
+const Component = {
+  view: v => {
+    return m('div', [
+      m('input[type=text]', {
+        oninput: m.withAttr('value', User.changeName),
+        value: User.name,
+      }),
+      m('h1', User.name),
+    ]);
+  },
+};
+
+const run = () => {
+  m.mount(document.body, Component);
+};
+run();
