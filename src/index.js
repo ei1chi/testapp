3d191ae373597d98943bd9c7b56126c0a3317846
eiichiro.takahashi <eiichiro.takahashi@o-11126-mac.local>
2018-08-21 17:44:57 +0900

    add some stuffs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import m from 'mithril';
-import 'spectre.css';
 
 let User = {
   name: 'aho',
   changeName: name => {
     User.name = name;
   },
+  count: 0,
 };
+
+class Param {
+  constructor(name, atk) {
+    this.name = name;
+    this.attack_pt = atk;
+  }
+}
+
+const Params = [
+  new Param('Taro', 100),
+  new Param('Jiro', 500),
+  new Param('Hanako', 50),
+];
 
 const Component = {
   view: v => [
@@ -15,9 +28,29 @@ const Component = {
         oninput: m.withAttr('value', User.changeName),
         value: User.name,
       }),
-      m('button', 'button1'),
-      m('h1', User.name),
+      m(
+        'button',
+        {
+          onclick: () => {
+            User.count++;
+          },
+        },
+        'push',
+      ),
+      m('h1', User.name + User.count),
     ]),
+    m('table', {border: 1}, [
+      m('thead', [m('th', 'name'), m('th', 'attack_pt')]),
+      m(
+        'tbody',
+        Params.map(p => [m('tr', [m('td', p.name), m('td', p.attack_pt)])]),
+      ),
+    ]),
+    m('hr'),
+    m(
+      'p',
+      ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    ),
   ],
 };
 
